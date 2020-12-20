c7a2bc7b720ca676016656ecaacc5afdd41d86be
cawleygl <grantlcawley@gmail.com>
2020-12-19 23:26:51 -0500

    Added funcitonality for city buttons

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ var cityButtonsEl = $("#city-buttons")
 var weatherEl = $("#weather-info")
 var forecastEl = $("#forecast")
 
-
-
 //Global variables
 var inputCity = ""
 
@@ -15,25 +13,50 @@ $("#submit-button").on("click", function (event) {
     inputCity = cityInputEl.val().trim();
 
     //Create new button element with new city name, re-write all buttons
-    var newButton = $("<a>");
+    var newButton = $("<button>");
     cityButtonsEl.append(newButton);
     newButton.text(inputCity)
-    newButton.addClass("collection-item")
-    newButton.attr("href", "#!")
+    newButton.attr("data-city", inputCity)
+    newButton.addClass("btn waves-effect waves-light")
+    var pgBreak = $("<br>");
+    cityButtonsEl.append(pgBreak);
+
 
     //run function to call and write weather/forecast data
     currentWeather();
+    console.log("submitted city from text")
 
 
 });
 
+$("#city-buttons").on("click", function (event) {
+    //Set inputCity variable to data value (city name)
+    event.preventDefault();
+
+    inputCity = ($(event.target).data("city"));
+    console.log("*" + inputCity + "*")
+
+    //run currentWeather again for given city
+    currentWeather();
+    console.log(inputCity + " button")
+
+
+});
+
+
 function currentWeather() {
+
+    //clear current weather and forecast info
+    weatherEl.empty();
+    forecastEl.empty();
+    console.log("cleared")
+
+
     //API
     var apiKey = "6577a14fe74e280aaf4799ecc064aac2"
 
     //set city name to lowercase
     inputCity = inputCity.toLowerCase();
-    console.log(inputCity);
 
     //current weather data for one location by city name
     var queryURL1 = "https://api.openweathermap.org/data/2.5/weather?q=" + inputCity + "&units=imperial&appid=" + apiKey
@@ -42,7 +65,7 @@ function currentWeather() {
         url: queryURL1,
         method: "GET"
     }).then(function (response) {
-        console.log(response)
+        console.log("call 1")
         //Create and append title with returned city name
         var cityName = $("<h3>");
         weatherEl.append(cityName);
@@ -77,7 +100,7 @@ function currentWeather() {
             url: queryURL2,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
+            console.log("call 2")
 
             //Create and append UV index list item in current weather
             var uv = $("<li>");
@@ -85,51 +108,51 @@ function currentWeather() {
             uv.text("UV Index: " + response.current.uvi)
 
             for (i = 0; i < 5; i++) {
-            //Create and append new forecast card
-            var forecastCard = $("<div>");
-            forecastEl.append(forecastCard)
-            forecastCard.addClass("card col s2 teal")
+                //Create and append new forecast card
+                var forecastCard = $("<div>");
+                forecastEl.append(forecastCard)
+                forecastCard.addClass("card col s2 teal")
 
-            var forecastPanel = $("<div>");
-            forecastCard.append(forecastPanel)
-            forecastPanel.addClass("card-content");
+                var forecastPanel = $("<div>");
+                forecastCard.append(forecastPanel)
+                forecastPanel.addClass("card-content");
 
-            //Create and append date container
-            var forecastDate = $("<span>");
-            forecastDate.addClass("card-title")
-            forecastPanel.append(forecastDate);
-            //Convert date input from unix and write to list
-            var unixTime = response.daily[i].dt;
-            console.log (unixTime);
-            milliseconds = unixTime * 1000;
-            console.log(milliseconds);
-            dateObject = new Date(milliseconds);
-            var month = dateObject.toLocaleString("en-US", {month: "numeric"});
-            var day = dateObject.toLocaleString("en-US", {day: "numeric"});
-            var year = dateObject.toLocaleString("en-US", {year: "numeric"});
-            forecastDate.text(month + "/" + day + "/" + year);
+                //Create and append date container
+                var forecastDate = $("<span>");
+                forecastDate.addClass("card-title")
+                forecastPanel.append(forecastDate);
+                //Convert date input from unix and write to list
+                var unixTime = response.daily[i].dt;
+                milliseconds = unixTime * 1000;
+                dateObject = new Date(milliseconds);
+                var month = dateObject.toLocaleString("en-US", { month: "numeric" });
+                var day = dateObject.toLocaleString("en-US", { day: "numeric" });
+                var year = dateObject.toLocaleString("en-US", { year: "numeric" });
+                forecastDate.text(month + "/" + day + "/" + year);
 
-            //Create and append temperature list item
-            var forecastTemp = $("<div>");
-            forecastPanel.append(forecastTemp);
-            forecastTemp.text(response.daily[i].temp.day + "°F")
+                //Create and append temperature list item
+                var forecastTemp = $("<div>");
+                forecastPanel.append(forecastTemp);
+                forecastTemp.text(response.daily[i].temp.day + "°F")
 
-            //Create and append humidity list item
-            var forecastHumid = $("<div>");
-            forecastPanel.append(forecastHumid);
-            forecastHumid.text(response.daily[i].humidity + "%")
+                //Create and append humidity list item
+                var forecastHumid = $("<div>");
+                forecastPanel.append(forecastHumid);
+                forecastHumid.text(response.daily[i].humidity + "%")
+
 
             }
 
+            inputCity = ""
+            console.log("reset inputCity")
+            console.log("----------------------------------------------")
+
         });
     });
-
-    //5-day forecast using log and lat from first call
-    // var log = ""
-    // var lat = ""
-
-    // var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + log + "&exclude=current,minutely,hourly,daily,alerts&appid=" + apiKey
-
-
 }
+
+
+
+
+
 
